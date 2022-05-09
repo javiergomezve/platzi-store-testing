4eefd4cc53255d09de300650e9daf2116267447e
Javier Gomez <javiergomezve@gmail.com>
2022-05-08 20:47:28 -0500

    [ADD] Testing actions

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,15 @@
+import actions from '../../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Actions', () => {
+  test('addToCart', () => {
+    const payload = ProductMock;
+
+    const expected = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+
+    expect(actions.addToCart(payload)).toEqual(expected);
+  });
+});
